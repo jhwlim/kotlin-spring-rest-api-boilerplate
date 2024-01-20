d2fdedbe78d8919abc80af40888c06dccc66c957
jhwlim <jhwlim25@gmail.com>
2024-01-20 22:34:28 +0900

    chore : 프로젝트 생성

---
+++ src/main/kotlin/com/example/restapiboilerplate/Application.kt
@@ -0,0 +1,11 @@
+package com.example.restapiboilerplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestApiBoilerplateApplication
+
+fun main(args: Array<String>) {
+    runApplication<RestApiBoilerplateApplication>(*args)
+}
