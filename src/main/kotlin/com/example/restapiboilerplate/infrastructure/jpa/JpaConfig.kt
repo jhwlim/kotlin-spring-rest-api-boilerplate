6de1a301441d8715c97f660a29b9302afce9e381
jhwlim <jhwlim25@gmail.com>
2024-02-14 16:48:10 +0900

    refactor : 클래스 위치 이동

--- src/main/kotlin/com/example/restapiboilerplate/infrastructure/jpa/JpaConfig.kt
+++ src/main/kotlin/com/example/restapiboilerplate/infrastructure/jpa/JpaConfig.kt
@@ -1,4 +1,4 @@
-package com.example.restapiboilerplate.config
+package com.example.restapiboilerplate.infrastructure.jpa
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
