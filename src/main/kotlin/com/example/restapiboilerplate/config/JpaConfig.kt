83323385cdc18f3f4601d8350277055613b4bec9
jhwlim <jhwlim25@gmail.com>
2024-02-18 22:35:52 +0900

    refactor : config 패키지 이동

--- src/main/kotlin/com/example/restapiboilerplate/config/JpaConfig.kt
+++ src/main/kotlin/com/example/restapiboilerplate/config/JpaConfig.kt
@@ -1,4 +1,4 @@
-package com.example.restapiboilerplate.infrastructure.jpa
+package com.example.restapiboilerplate.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
