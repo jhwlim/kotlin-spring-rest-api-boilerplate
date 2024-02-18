0878b63ee80a1fd05307f2320b51c0a3c296b3c1
jhwlim <jhwlim25@gmail.com>
2024-02-18 23:05:03 +0900

    config : Security 기본 설정 추가

---
+++ src/main/kotlin/com/example/restapiboilerplate/config/SecurityConfig.kt
@@ -0,0 +1,48 @@
+package com.example.restapiboilerplate.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .csrf {
+                it.disable()
+            }
+            .sessionManagement {
+                it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+            .formLogin {
+                it.disable()
+            }
+            .logout {
+                it.disable()
+            }
+            .authorizeHttpRequests {
+                it.requestMatchers(*PERMIT_ALL_PATHS.toTypedArray()).permitAll()
+            }
+            .build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    companion object {
+
+        private val PERMIT_ALL_PATHS = listOf(
+            "/v1/users",
+        )
+
+    }
+
+}
