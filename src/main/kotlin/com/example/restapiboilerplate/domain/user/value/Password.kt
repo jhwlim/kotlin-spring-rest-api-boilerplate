4de389e241be3906c1905a118515a83746191026
jhwlim <jhwlim25@gmail.com>
2024-02-18 22:49:48 +0900

    feat : Password 수정
    - 패턴 validation 삭제
    - toString() 호출시 value 가 그대로 노출 안되도록 처리

--- src/main/kotlin/com/example/restapiboilerplate/domain/user/value/Password.kt
+++ src/main/kotlin/com/example/restapiboilerplate/domain/user/value/Password.kt
@@ -1,21 +1,18 @@
 package com.example.restapiboilerplate.domain.user.value
 
-import java.util.regex.Pattern
-
 @JvmInline
 value class Password(
     private val value: String
 ) {
 
-    init {
-        val matcher = PATTERN.matcher(value)
-
-        require(matcher.matches()) { "비밀번호 형식이 올바르지 않습니다." }
+    override fun toString(): String {
+        return CHAR.repeat(value.length)
     }
 
     companion object {
-        private const val REGEX = "^[0-9]*\$"
-        private val PATTERN = Pattern.compile(REGEX)
+
+        private const val CHAR = "*"
+
     }
 
 }
--- src/test/kotlin/com/example/restapiboilerplate/domain/user/value/PasswordTest.kt
+++ src/test/kotlin/com/example/restapiboilerplate/domain/user/value/PasswordTest.kt
@@ -5,31 +5,18 @@ import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.data.forAll
 import io.kotest.data.row
+import io.kotest.matchers.shouldBe
 
 class PasswordTest : DescribeSpec({
 
-    describe("init") {
+    describe("toString") {
 
-        context("숫자가 주어진 경우") {
+        context("문자열이 주어진 경우") {
 
-            it("예외가 발생하지 않아야 한다.") {
-                shouldNotThrowAny { Password("1234567890") }
-            }
+            val actual = Password("123456")
 
-        }
-
-        context("숫자가 아닌 문자가 주어진 경우") {
-
-            forAll(
-                row("영문이 포함된 경우", "1234a"),
-                row("한글이 포함된 경우", "1234가"),
-                row("특수문자가 포함된 경우", "1234@"),
-            ) { description, value ->
-
-                it("$description - 예외가 발생해야 한다.") {
-                    shouldThrowIllegalArgumentExceptionWithMessage("비밀번호 형식이 올바르지 않습니다.") { Password(value) }
-                }
-
+            it("'*' 가 주어진 문자열의 길이만큼 반환해야 한다.") {
+                actual.toString() shouldBe "******"
             }
 
         }
